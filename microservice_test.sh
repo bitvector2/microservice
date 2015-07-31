437261e487f16a1bdb5bf12e5f349e6cde29fe8b
Steven Logue <bitvector2@gmail.com>
2015-07-30 19:35:21 -0700

    removed -d64 parameter from runner script for Raspberry Pi 2 compatibility

--- microservice_test.sh
+++ microservice_test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -d64 -server -XX:+UseG1GC -jar build/libs/microservice_test-1.0-SNAPSHOT-all.jar
+java -server -XX:+UseG1GC -jar build/libs/microservice_test-1.0-SNAPSHOT-all.jar
