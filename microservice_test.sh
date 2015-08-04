5e006876e334aae3692c2f9687ab8b83c77b4042
Steven Logue <bitvector2@gmail.com>
2015-08-04 13:09:10 -0700

    modified startup script to handle Raspberry Pi as a special case

--- microservice_test.sh
+++ microservice_test.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-java -d64 -server -jar build/libs/microservice_test-1.0-SNAPSHOT-all.jar
-
+if [ $(uname -m) == "armv7l" ];
+then
+    java -d32 -server -jar build/libs/microservice_test-1.0-SNAPSHOT-all.jar
+else
+    java -d64 -server -jar build/libs/microservice_test-1.0-SNAPSHOT-all.jar
+fi
