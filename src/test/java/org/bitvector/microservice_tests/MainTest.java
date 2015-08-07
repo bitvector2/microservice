fd91fa915c861cd0ac228848e68465c3428c4e61
Steven Logue <bitvector2@gmail.com>
2015-08-07 15:06:40 -0700

    initial add of junit and testing framework

--- src/test/java/org/bitvector/microservice_tests/MainTest.java
+++ src/test/java/org/bitvector/microservice_tests/MainTest.java
@@ -1,13 +1,17 @@
 package org.bitvector.microservice_tests;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.Assert.assertEquals;
 
 public class MainTest {
+    Logger logger = LoggerFactory.getLogger("org.bitvector.microservice.MainTest");
+
     @Test
     public void evaluatesExpression() {
         assertEquals(6, 6);
-        System.out.println("A Test Ran");
+        logger.info("A Test Ran");
     }
 }
