040cd4a851ee3694d79e2e0a3ef0ad1b8dc2728e
Steven Logue <bitvector2@gmail.com>
2015-08-07 17:24:09 -0700

    Update microservice.sh

--- microservice.sh
+++ microservice.sh
@@ -2,7 +2,7 @@
 
 if [ $(uname -m) == "armv7l" ];
 then
-    java -d32 -server -XX:+UseG1GC -jar build/libs/microservice_test-1.0-SNAPSHOT-all.jar
+    java -d32 -server -XX:+UseG1GC -jar build/libs/microservice-1.0-SNAPSHOT-all.jar
 else
-    java -d64 -server -XX:+UseG1GC -jar build/libs/microservice_test-1.0-SNAPSHOT-all.jar
+    java -d64 -server -XX:+UseG1GC -jar build/libs/microservice-1.0-SNAPSHOT-all.jar
 fi
